db0284b51df40dcbbf76815114a86c24e2f87bae
Jason Florentino <jasonmflorentino@gmail.com>
2021-02-15 14:55:03 -0500

    Add Get route to /api
    Create super simple endpoint that will get the data from the database and send it back to the client

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.use(express.json({ limit: '1mb' }));
 const database = new Datastore('database.db');
 database.loadDatabase();
 
+app.get('/api', (request, response) => {
+	database.find({}, (err, data) => {
+		if (err) {
+			response.end();
+			return;
+		}
+		response.json(data);
+	})
+})
+
 app.post('/api', (request, response) => {
 	console.log("I got a request!");
 	const data = request.body;
