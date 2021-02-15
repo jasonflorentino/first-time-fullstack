d462d47321a497178b3d1958bdefca3ae13cda16
Jason Florentino <jasonmflorentino@gmail.com>
2021-02-15 14:28:29 -0500

    Added nedb to index.js

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 app.listen(3000, () => console.log('Listening at 3000...'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.post('/api', (request, response) => {
 	console.log("I got a request!");
-	console.log(request.body);
+	const data = request.body;
+	const timestamp = Date.now();
+	data.timestamp = timestamp;
+	database.insert(data);
 	response.json({
-		reply: 'Got your message!',
-		message: request.body
+		reply: 'success',
+		timestamp: timestamp,
+		message: data
 	});
-	console.log("Sent a response!");
 });
